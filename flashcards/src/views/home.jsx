22bebb171ce3dc5a5eda56e4595a5779dbb4707a
Sai Kumar Mamidala <sk594068@dal.ca>
2025-04-10 19:20:17 -0300

    Fix: updated CloudFormation

--- flashcards/src/views/home.jsx
+++ flashcards/src/views/home.jsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
 import { Button, Container, Form, Row, Col, Card } from "react-bootstrap";
-import { PdfReader } from "pdfreader";
-
-new PdfReader().parseFileItems("test/sample.pdf", (err, item) => {
-  if (err) console.error("error:", err);
-  else if (!item) console.warn("end of file");
-  else if (item.text) console.log(item.text);
-});
 
 const Home = () => {
     const [pdfFile, setPdfFile] = useState(null);
